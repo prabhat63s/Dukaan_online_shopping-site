55217a2903654181e2329726d72431b51bed8ee5
prabhat63s <prabhat6914@gmail.com>
2024-09-23 20:40:34 +0530

    server side issue fix

--- server/server.js
+++ server/server.js
@@ -24,8 +24,6 @@ const __dirname = path.resolve();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname, './client/build')));
-
 
 //routes
 app.use("/api/v1/auth", authRoutes);
